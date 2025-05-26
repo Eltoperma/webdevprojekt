4f4734c044aa282b08972e85cbdf71ffdba81a8f
Marten Schnack <marten.schnack@freenet.ag>
2025-05-26 10:41:03 +0200

    Namensänderung UserDTO

--- types/userDTO.ts
+++ types/userDTO.ts
@@ -1,11 +1,7 @@
-export interface User {
+export interface UserDTO {
   id?: string;
-  auth_user_id?: string;
   name?: string;
   created_at?: string;
   updated_at?: string;
   email?: string;
-  auth_user?: {
-    email?: string;
-  };
 }
