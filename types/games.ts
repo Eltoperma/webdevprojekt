4e87e583ed3a4ec0ad82b57b8e90f6ce7b3bb8b6
Marten Schnack <marten.schnack@freenet.ag>
2025-06-02 10:15:08 +0200

    Landing Page in client und server side getrennt. DB Zugriffe auf die tabelle "game"

---
+++ types/games.ts
@@ -0,0 +1,13 @@
+export const games = [
+  {
+    title: "Galacy Trader",
+    href: "/games/galaxy-trader",
+    description:
+      "Galaxien erforschen und durch Glück und Geschick ökonomischen Erfolg erzielen...",
+  },
+  {
+    title: "MindMaze",
+    href: "/games/mind-maze",
+    description: "Teste dein Wissen in einem täglichen Rätsel.",
+  },
+];
