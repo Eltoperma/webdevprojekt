23d98881469d7f857cb01c537969a16c104363f0
Eltoperma <91690410+Eltoperma@users.noreply.github.com>
2025-06-02 15:32:40 +0200

    removed prisma

--- app/mathgame/page.tsx
+++ app/mathgame/page.tsx
@@ -1,9 +1,10 @@
 import MathGame from "../components/MathGame/MathGame";
+import { createSupabaseServerClient } from "../lib/supabase/supabaseServerClient";
 
-export default function MathGamePage() {
+export default async function MathGamePage() {
   return (
     <main className="min-h-screen bg-gray-50 dark:bg-gray-900">
-      <MathGame />
+      <MathGame  />
     </main>
   );
 }
